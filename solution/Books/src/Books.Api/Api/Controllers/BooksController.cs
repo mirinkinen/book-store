b0846be16b972c728824f8637b60be52ace20d71
Mika Rinkinen <mirinkinen@gmail.com>
2023-06-11 18:42:04 +0300

    Add BooksController

---
+++ solution/Books/src/Books.Api/Api/Controllers/BooksController.cs
@@ -0,0 +1,22 @@
+﻿using Books.Api.Domain.Books;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Books.Api.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public BooksController()
+        { }
+
+        [HttpGet]
+        public IEnumerable<Book> GetBooks()
+        {
+            return new List<Book> {
+                new Book("Hello world", DateTime.UtcNow, Guid.NewGuid()),
+                new Book("Hello world", DateTime.UtcNow, Guid.NewGuid())
+            };
+        }
+    }
+}
