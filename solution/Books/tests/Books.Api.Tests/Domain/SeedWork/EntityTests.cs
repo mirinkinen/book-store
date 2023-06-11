337a870b408f6a627c4c2e783f9a87207bb5240e
Mika Rinkinen <mirinkinen@gmail.com>
2023-06-11 18:04:42 +0300

    Verify that entities don't have public setters

--- solution/Books/tests/Books.Api.Tests/Domain/SeedWork/EntityTests.cs
+++ solution/Books/tests/Books.Api.Tests/Domain/SeedWork/EntityTests.cs
@@ -8,6 +8,17 @@ namespace Books.Api.Tests.Domain.SeedWork
     public class EntityTests
     {
         [Fact]
+        public void Entities_DoesNotHavePublicSetters()
+        {
+            typeof(Entity).Assembly.GetTypes()
+                .Where(type => typeof(Entity).IsAssignableFrom(type))
+                .SelectMany(type => type.GetProperties())
+                .Select(propertyInfo => propertyInfo.GetSetMethod())
+                .Where(methodInfo => methodInfo != null && methodInfo.IsPublic)
+                .Should().BeEmpty("domain entities should not have public setters");
+        }
+
+        [Fact]
         public void Entity_WhenCreated_HasId()
         {
             var entity = (Entity)new Book("test", new DateTime(2020, 1, 1), Guid.NewGuid());
