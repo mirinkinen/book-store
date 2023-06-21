9e010efc39d01656aea8fc18cdc56a8c5dbbc424
Mika Rinkinen <mirinkinen@gmail.com>
2023-06-21 21:46:31 +0300

    Protect domain entities's internal state better

--- solution/Books/src/Books.Domain/Authors/Author.cs
+++ solution/Books/src/Books.Domain/Authors/Author.cs
@@ -1,22 +1,28 @@
 ﻿using Books.Domain.Books;
 using Books.Domain.SeedWork;
+using System.Collections.ObjectModel;
 
 namespace Books.Domain.Authors;
 
 public class Author : Entity
 {
-    public string Firstname { get; internal set; }
+    public string Firstname { get; private set; }
 
-    public string Lastname { get; internal set; }
+    public string Lastname { get; private set; }
 
-    public DateTime Birthday { get; internal set; }
+    public DateTime Birthday { get; private set; }
 
-    public IList<Book> Books { get; internal set; } = new List<Book>();
+    public ReadOnlyCollection<Book> Books { get; private set; } = new List<Book>().AsReadOnly();
 
-    public Guid OrganizationId { get; internal set; }
+    public Guid OrganizationId { get; private set; }
 
     public Author(string firstname, string lastname, DateTime birthday, Guid organizationId)
     {
+        if(organizationId == Guid.Empty)
+        {
+            throw new DomainRuleException("Empty organization ID not allowed.");
+        }
+
         Firstname = firstname;
         Lastname = lastname;
         Birthday = birthday;
--- solution/Books/src/Books.Domain/Books/Book.cs
+++ solution/Books/src/Books.Domain/Books/Book.cs
@@ -5,13 +5,13 @@ namespace Books.Domain.Books
 {
     public class Book : Entity
     {
-        public string Title { get; internal set; }
+        public string Title { get; private set; }
 
-        public DateTime DatePublished { get; internal set; }
+        public DateTime DatePublished { get; private set; }
 
-        public Author Author { get; internal set; }
+        public Author Author { get; private set; }
 
-        public Guid AuthorId { get; internal set; }
+        public Guid AuthorId { get; private set; }
 
         public Book(string title, DateTime datePublished, Guid authorId) : base()
         {
