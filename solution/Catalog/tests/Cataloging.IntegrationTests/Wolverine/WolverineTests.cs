117d7431db1bf709f583cc9cdc051a961552a66c
Mika Rinkinen <mirinkinen@gmail.com>
2023-07-04 12:15:26 +0300

    Add test for Wolverine configuration

---
+++ solution/Catalog/tests/Cataloging.IntegrationTests/Wolverine/WolverineTests.cs
@@ -0,0 +1,13 @@
+﻿namespace Cataloging.IntegrationTests.Wolverine;
+
+public class WolverineTests
+{
+    [Fact]
+    [Trait("Category", "Wolverine")]
+    public void WolverineConfigurationIsValid()
+    {
+        using var factory = new WolverineValidationFactory();
+        // Starts application just to execute the AssertWolverineConfigurationIsValid method.
+        using var client = factory.CreateClient();
+    }
+}
---
+++ solution/Catalog/tests/Cataloging.IntegrationTests/Wolverine/WolverineValidationFactory.cs
@@ -0,0 +1,17 @@
+﻿using Cataloging.Api;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+using Wolverine;
+
+namespace Cataloging.IntegrationTests;
+
+public class WolverineValidationFactory : WebApplicationFactory<Program>
+{
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        var host = base.CreateHost(builder);
+        host.AssertWolverineConfigurationIsValid();
+
+        return host;
+    }
+}
