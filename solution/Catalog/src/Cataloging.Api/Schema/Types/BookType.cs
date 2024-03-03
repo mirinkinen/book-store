170fa2dc1240137574a446646e053c89ff45192c
Mika Rinkinen <mirinkinen@gmail.com>
2024-03-03 17:27:17 +0200

    Add author to book type (naive implementation)

--- solution/Catalog/src/Cataloging.Api/Schema/Types/BookType.cs
+++ solution/Catalog/src/Cataloging.Api/Schema/Types/BookType.cs
@@ -1,5 +1,8 @@
 using Cataloging.Domain.Books;
+using Cataloging.Infrastructure.Database;
+using GraphQL;
 using GraphQL.Types;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cataloging.Api.Schema.Types;
 
@@ -12,5 +15,14 @@ public class BookType : ObjectGraphType<Book>
         Field(b => b.Title).Description("The title of the book");
         Field(b => b.DatePublished).Description("The date when the book was published");
         Field(b => b.AuthorId).Description("The ID of the author of the book");
+
+        Field<AuthorType>("author", resolve: GetAuthor);
+    }
+
+    private object? GetAuthor(IResolveFieldContext<Book> context)
+    {
+        var dbContext = context.RequestServices!.GetRequiredService<CatalogDbContext>();
+
+        return dbContext.Authors.FirstOrDefault(a => a.Id == context.Source.AuthorId);
     }
 }
