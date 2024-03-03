4ecc0eabcd20d216285eb263561052c984802e4e
Mika Rinkinen <mirinkinen@gmail.com>
2024-03-03 17:08:22 +0200

    Add more fields to book

--- solution/Catalog/src/Cataloging.Api/Schema/Types/BookType.cs
+++ solution/Catalog/src/Cataloging.Api/Schema/Types/BookType.cs
@@ -7,8 +7,10 @@ public class BookType : ObjectGraphType<Book>
 {
     public BookType()
     {
-        Field(b => b.Id);
-        Field(b => b.Price);
-        Field(b => b.Title);
+        Field(b => b.Id).Description("The ID of the book");
+        Field(b => b.Price).Description("The price of the book");
+        Field(b => b.Title).Description("The title of the book");
+        Field(b => b.DatePublished).Description("The date when the book was published");
+        Field(b => b.AuthorId).Description("The ID of the author of the book");
     }
 }
