2998c1c57c6eac0d2e8de116d113a5c896fe86a0
Mika Rinkinen <mirinkinen@gmail.com>
2025-02-22 16:38:05 +0200

    Add architecture tests

---
+++ solution/Catalog/tests/Cataloging.ArchitectureTests/LayerTests.cs
@@ -0,0 +1,48 @@
+using FluentAssertions;
+using NetArchTest.Rules;
+using System.Reflection;
+
+namespace Cataloging.ArchitectureTests;
+
+public class LayerTests
+{
+    private readonly Assembly _assembly;
+
+    public LayerTests()
+    {
+        _assembly = typeof(ServiceConfigurator).Assembly;
+    }
+
+    [Fact]
+    public void Assembly_should_not_be_empty()
+    {
+        Types.InAssembly(_assembly).GetTypes().Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public void Domain_layer_should_not_depend_on_anything()
+    {
+        Types.InAssembly(_assembly).That().ResideInNamespaceStartingWith("Cataloging.Domain")
+            .Should().NotHaveDependencyOn("Cataloging.API")
+            .And().NotHaveDependencyOn("Cataloging.Application")
+            .And().NotHaveDependencyOn("Cataloging.Infra")
+            .GetResult().FailingTypeNames.Should().BeNullOrEmpty();
+    }
+
+    [Fact]
+    public void Application_layer_should_not_depend_on_infra_or_API()
+    {
+        Types.InAssembly(_assembly).That().ResideInNamespaceStartingWith("Cataloging.Application")
+            .Should().NotHaveDependencyOn("Cataloging.Infra")
+            .And().NotHaveDependencyOn("Cataloging.API")
+            .GetResult().FailingTypeNames.Should().BeNullOrEmpty();
+    }
+    
+    [Fact]
+    public void Infra_layer_should_not_depend_on_API()
+    {
+        Types.InAssembly(_assembly).That().ResideInNamespaceStartingWith("Cataloging.Infra")
+            .Should().NotHaveDependencyOn("Cataloging.API")
+            .GetResult().FailingTypeNames.Should().BeNullOrEmpty();
+    }
+}
