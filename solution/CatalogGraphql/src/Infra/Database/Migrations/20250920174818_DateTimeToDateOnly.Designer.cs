f0f14376f71146500fbf1c8ef533e3eff800ff39
Mika Rinkinen <mirinkinen@gmail.com>
2025-09-20 20:48:48 +0300

    Create migration for DateOnly refactoring

--- solution/CatalogGraphql/src/Infra/Database/Migrations/20250920174818_DateTimeToDateOnly.Designer.cs
+++ solution/CatalogGraphql/src/Infra/Database/Migrations/20250920174818_DateTimeToDateOnly.Designer.cs
@@ -4,16 +4,19 @@ using Infra.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace Infra.Data.Migrations
+namespace Infra.Database.Migrations
 {
     [DbContext(typeof(CatalogDbContext))]
-    partial class CatalogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250920174818_DateTimeToDateOnly")]
+    partial class DateTimeToDateOnly
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
