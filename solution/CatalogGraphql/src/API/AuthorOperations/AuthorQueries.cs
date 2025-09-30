4f71f8d8ab24d0dddef4aeb887961f0029ac1d99
Mika Rinkinen <mirinkinen@gmail.com>
2025-09-30 20:48:41 +0300

    Remoe author and book entities from API

--- solution/CatalogGraphql/src/API/AuthorOperations/AuthorQueries.cs
+++ solution/CatalogGraphql/src/API/AuthorOperations/AuthorQueries.cs
@@ -2,21 +2,15 @@ using Application.AuthorQueries;
 using Application.AuthorQueries.GetAuthorById;
 using Application.AuthorQueries.GetAuthors;
 using Common.Domain;
-using Domain;
 using GreenDonut.Data;
 using HotChocolate.Types.Pagination;
-using Infra.Data;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
-using System.Diagnostics;
 
 namespace API.AuthorOperations;
 
 [QueryType]
 public static partial class AuthorQueries
 {
-    private static readonly ActivitySource _activity = new(nameof(AuthorQueries));
-    
     [NodeResolver]
     [Error<EntityNotFoundException>]
     public static async Task<AuthorDto> GetAuthorById(Guid id, ISender sender)
@@ -35,25 +29,5 @@ public static partial class AuthorQueries
     {
         var page = await sender.Send(new GetAuthorsQuery(pagingArguments, queryContext), cancellationToken);
         return new PageConnection<AuthorDto>(page);
-    }
-    
-    [UseConnection]
-    [UseFiltering]
-    [UseSorting]
-    public static async Task<PageConnection<Author>> GetAuthorEntities(
-        PagingArguments pagingArguments,
-        QueryContext<Author> queryContext,
-        IDbContextFactory<CatalogDbContext> dbContextFactory,
-        CancellationToken cancellationToken)
-    {
-        using var activity = _activity.StartActivity();
-
-        await using var dbContext = await dbContextFactory.CreateDbContextAsync(cancellationToken);
-        var page = await dbContext.Authors
-            .Include(a => a.Books)
-            .With(queryContext, sort => sort.IfEmpty(o => o.AddDescending(t => t.Id)))
-            .ToPageAsync(pagingArguments, cancellationToken);
-        
-        return new PageConnection<Author>(page);
     }
 }
--- solution/CatalogGraphql/src/API/BookOperations/BookQueries.cs
+++ solution/CatalogGraphql/src/API/BookOperations/BookQueries.cs
@@ -2,21 +2,15 @@ using Application.BookQueries;
 using Application.BookQueries.GetBookById;
 using Application.BookQueries.GetBooks;
 using Common.Domain;
-using Domain;
 using GreenDonut.Data;
 using HotChocolate.Types.Pagination;
-using Infra.Data;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
-using System.Diagnostics;
 
 namespace API.BookOperations;
 
 [QueryType]
 public static partial class BookQueries
 {
-    private static readonly ActivitySource _activity = new(nameof(BookQueries));
-
     [NodeResolver]
     [Error<EntityNotFoundException>]
     public static async Task<BookDto> GetBookById(Guid id, ISender sender)
@@ -33,29 +27,7 @@ public static partial class BookQueries
         ISender sender,
         CancellationToken cancellationToken)
     {
-        using var activity = _activity.StartActivity();
-
         var page = await sender.Send(new GetBooksQuery(pagingArguments, queryContext), cancellationToken);
         return new PageConnection<BookDto>(page);
-    }
-    
-    [UseConnection]
-    [UseFiltering]
-    [UseSorting]
-    public static async Task<PageConnection<Book>> GetBookEntities(
-        PagingArguments pagingArguments,
-        QueryContext<Book> queryContext,
-        IDbContextFactory<CatalogDbContext> dbContextFactory,
-        CancellationToken cancellationToken)
-    {
-        using var activity = _activity.StartActivity();
-
-        await using var dbContext = await dbContextFactory.CreateDbContextAsync(cancellationToken);
-        
-        var page = await dbContext.Books
-            .With(queryContext, sort => sort.IfEmpty(s => s.AddDescending(b => b.Id)))
-            .ToPageAsync(pagingArguments, cancellationToken);
-        
-        return new PageConnection<Book>(page);
     }
 }
